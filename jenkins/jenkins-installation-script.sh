10441a621bccf012a0f102ca44bcdfe68e04fadc
Vandananm08 <109121694+Vandananm08@users.noreply.github.com>
2024-04-28 16:38:21 +0530

    Update jenkins-installation-script.sh

--- jenkins/jenkins-installation-script.sh
+++ jenkins/jenkins-installation-script.sh
@@ -8,22 +8,22 @@ wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bi
 unzip apache-maven-3.9.6-bin.zip
 sudo cp -r apache-maven-3.9.6 /opt/
 
-echo "#SET UP JAVA AND MAVEN" >> .bashrc
+echo "#SET UP JAVA AND MAVEN" >> .bashrc #.bashrc contains terminal configurations
 echo "JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64" >> .bashrc
 echo "M2_HOME=/opt/apache-maven-3.9.6/bin" >> .bashrc
-echo "export JAVA_HOME" >> .bashrc
+echo "export JAVA_HOME" >> .bashrc  #export will make JAVA_HOME variable environment variable globally
 echo "export M2_HOME" >> .bashrc
-echo "PATH=$PATH:$JAVA_HOME:$M2_HOME" >> .bashrc
-echo "export PATH" >> .bashrc
+echo "PATH=$PATH:$JAVA_HOME:$M2_HOME" >> .bashrc #PATH variable stores any defined paths of shell commands globally
+echo "export PATH" >> .bashrc #saving new path 
 
-source .bashrc
+source .bashrc #reload .bashrc (bash is a shell and rc is remote control)
 
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo wget -O /etc/yum.repos.d/.jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo #-O in wget we can define path where particular file can be donwloaded
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key #key is required to read the repo
 
 sudo yum -y install jenkins
 sudo service jenkins start
-sudo chkconfig jenkins on
+sudo chkconfig jenkins on  #chkconfig which will ensure jenkins services is always on
 
 echo "#SETTING UP JENKINS WORKSPACE" >> .bashrc
 echo "JENKINS_WORKSPACE=/var/lib/jenkins/workspace" >> .bashrc
